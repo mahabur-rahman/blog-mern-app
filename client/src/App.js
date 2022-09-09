ac1c3b5a0a52e8847e56aa8f54022328e0e0f8af
mahabur  rahman <annur4395@gmail.com>
2022-09-09 15:37:05 +0600

    topbar added

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
+import "./global.css";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+// components
+import Topbar from "./components/Topbar/Topbar";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Router>
+        <Topbar />
+      </Router>
+    </>
+  );
 };
 
 export default App;
