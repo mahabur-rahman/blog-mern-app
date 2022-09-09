b5055161e324f89176aaa8f183c193be6a642811
mahabur  rahman <annur4395@gmail.com>
2022-09-09 19:07:17 +0600

    connection create

--- api/index.js
+++ api/index.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const app = express();
+const colors = require("colors");
 
 // config env
 const dotenv = require("dotenv");
 dotenv.config();
 
+// connected to db
+const connectedDB = require("./db/connect");
+connectedDB();
+
 // listen app
 app.listen(5000, () => {
-  console.log(`Server is running..`);
+  console.log(`Server is running...`);
 });
