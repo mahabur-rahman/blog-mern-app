5d87e818eb600daf4c98f8fc885bf4c47a48dc74
mahabur  rahman <annur4395@gmail.com>
2022-09-09 19:17:21 +0600

    user model declatre

---
+++ api/models/User.model.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+// userSchema
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+    },
+
+    profilePic: {
+      type: String,
+      default: "",
+    },
+  },
+  { timestamps: true }
+);
+
+// create a collection | Model
+const UserModel = mongoose.model("User", UserSchema);
