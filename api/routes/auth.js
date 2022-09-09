2416de7a4ab99262071ed48a7224f2b1dd874773
mahabur  rahman <annur4395@gmail.com>
2022-09-09 21:08:36 +0600

    user login controller added

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -25,5 +25,28 @@ router.post("/register", async (req, res) => {
 
 // LOGIN
 
+router.post("/login", async (req, res) => {
+  try {
+    const user = await UserModel.findOne({ username: req.body.username });
+
+    !user && res.status(400).json("Wrong credentials!");
+
+    const validatedPass = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+
+    !validatedPass && res.status(400).json("Wrong Credentials!");
+
+    const { password, ...others } = user._doc;
+
+    return res.status(200).json(others);
+
+    console.log(user);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
+
 // export
 module.exports = router;
