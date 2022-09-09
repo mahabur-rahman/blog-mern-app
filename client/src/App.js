d84a7cc9b49bd3b45b283b009cdd6ea522297fa2
mahabur  rahman <annur4395@gmail.com>
2022-09-09 15:45:58 +0600

    header component added

--- client/src/App.js
+++ client/src/App.js
@@ -5,12 +5,19 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 // components
 import Topbar from "./components/Topbar/Topbar";
+import Home from "./pages/Home/Home";
 
 const App = () => {
   return (
     <>
       <Router>
         <Topbar />
+
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </Switch>
       </Router>
     </>
   );
