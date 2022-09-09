62c26b9accabd3ab16ff2328ecf447ed9d67ced2
mahabur  rahman <annur4395@gmail.com>
2022-09-09 19:22:30 +0600

    post model added

---
+++ api/models/Post.model.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+// PostSchema
+const PostSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+
+    desc: {
+      type: String,
+      required: true,
+    },
+    photo: {
+      type: String,
+      required: false,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+
+    categories: {
+      type: Array,
+      required: false,
+    },
+  },
+  { timestamps: true }
+);
+
+// create a collection | Model
+const PostModel = mongoose.model("Post", PostSchema);
